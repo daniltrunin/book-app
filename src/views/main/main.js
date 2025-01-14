714c07c6adc168fa6f057fde2256d2313ff6e0e3
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-01-14 21:43:27 +0500

    INITIAL COMMIT

---
+++ src/views/main/main.js
@@ -0,0 +1,36 @@
+import onChange from "on-change";
+import { AbstractView } from "../../common/view";
+import { Header } from "../../components/header/header";
+
+export class MainView extends AbstractView {
+    state = {
+        list: [],
+        loading: false,
+        searchQuery: undefined,
+        offset: 0
+    };
+
+    constructor(appState) {
+        super();
+        this.appState = appState;
+        this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.setTitle("Поиск книг")
+    }
+
+    appStateHook(path) {
+        console.log(path)
+    }
+
+    render() {
+        const main = document.createElement("div");
+        this.app.innerHTML = "";
+        this.app.append(main);
+        this.renderHeader();
+        this.appState.favorites.push("Star Wars");
+    }
+
+    renderHeader() {
+        const header = new Header(this.appState).render();
+        this.app.prepend(header);
+    }
+}
